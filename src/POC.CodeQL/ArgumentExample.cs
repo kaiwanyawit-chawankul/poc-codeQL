65cc6a92cc874e7e965c5fa8faa71e7c92a871af
Kaiwanyawit Chawankul <kaiwanyawit.c@thoughtworks.com>
2024-10-01 14:39:13 +0700

    add This method has too many parameters.

---
+++ src/POC.CodeQL/ArgumentExample.cs
@@ -0,0 +1,21 @@
+using System;
+
+public class ArgumentExample
+{
+    public void ProcessData(
+        int param1, int param2, int param3, int param4, int param5,
+        int param6, int param7, int param8, int param9, int param10,
+        int param11, int param12, int param13, int param14, int param15,
+        int param16, int param17, int param18, int param19, int param20,
+        int param21, int param22, int param23, int param24, int param25,
+        int param26, int param27)
+    {
+        Console.WriteLine("Processing data with multiple parameters:");
+        Console.WriteLine($"Param1: {param1}, Param2: {param2}, Param3: {param3}, Param4: {param4}, Param5: {param5}");
+        Console.WriteLine($"Param6: {param6}, Param7: {param7}, Param8: {param8}, Param9: {param9}, Param10: {param10}");
+        Console.WriteLine($"Param11: {param11}, Param12: {param12}, Param13: {param13}, Param14: {param14}, Param15: {param15}");
+        Console.WriteLine($"Param16: {param16}, Param17: {param17}, Param18: {param18}, Param19: {param19}, Param20: {param20}");
+        Console.WriteLine($"Param21: {param21}, Param22: {param22}, Param23: {param23}, Param24: {param24}, Param25: {param25}");
+        Console.WriteLine($"Param26: {param26}, Param27: {param27}");
+    }
+}
